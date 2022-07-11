b4fd79a0ba7c3c54cab021ab9877f9de73585a5a
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2022-07-11 14:00:14 +0200

    Detect locale on page load

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useCallback } from "react";
+import { useCallback, useEffect } from "react";
 import Particles from "react-tsparticles";
 import type { Engine } from "tsparticles-engine";
 import { loadFull } from "tsparticles";
@@ -9,10 +9,16 @@ import Header from "./components/sections/header/Header";
 import Main from "./components/sections/main/Main";
 import Footer from "./components/sections/footer/Footer";
 import Works from "./components/sections/work/Works";
+import i18n from "./i18n";
 
 function App(): JSX.Element {
   const particlesInit = useCallback(async (engine: Engine) => {
     await loadFull(engine);
+  }, []);
+
+  useEffect(() => {
+    // Detect locale on site load
+    i18n.changeLanguage(navigator.language.split("-")[0]);
   }, []);
 
   return (
