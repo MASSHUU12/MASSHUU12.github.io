3d637c5f838f1bcbc6dabdcdf49fce643a108cf8
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2022-11-17 17:57:02 +0100

    Refreshed design

---
+++ src/components/common/Btn.tsx
@@ -0,0 +1,35 @@
+import { useSpring, animated, config } from "@react-spring/web";
+
+interface Props {
+  text: string;
+  action: (args: any) => any;
+  delay?: number;
+}
+
+const Btn = ({ text, action, delay = 0 }: Props): JSX.Element => {
+  const animConfig = useSpring({
+    to: {
+      opacity: 1,
+      y: 0,
+    },
+    from: {
+      opacity: 0,
+      y: 100,
+    },
+    config: config.slow,
+    delay: delay,
+  });
+
+  return (
+    <animated.button
+      type="button"
+      style={animConfig}
+      className="common-btn"
+      onClick={action}
+    >
+      {text}
+    </animated.button>
+  );
+};
+
+export default Btn;
