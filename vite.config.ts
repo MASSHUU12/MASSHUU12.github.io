001c36d6ac232af5d5220c5e2bf223f2b493251d
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-01-18 18:08:41 +0100

    Changed prettier config format to json

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import preact from '@preact/preset-vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
-import path from 'path'
+import path from "path";
+import { defineConfig } from "vite";
+import preact from "@preact/preset-vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,12 +9,12 @@ export default defineConfig({
   plugins: [preact(), tsconfigPaths()],
   resolve: {
     alias: {
-      src: path.resolve('src/'),
-      cmp: path.resolve('./src/components'),
-      common: path.resolve('./src/components/common'),
-      sections: path.resolve('./src/components/sections'),
-      utils: path.resolve('./src/utils'),
-      features: path.resolve('./src/features')
+      src: path.resolve("src/"),
+      cmp: path.resolve("./src/components"),
+      common: path.resolve("./src/components/common"),
+      sections: path.resolve("./src/components/sections"),
+      utils: path.resolve("./src/utils"),
+      features: path.resolve("./src/features"),
     },
-  }
-})
+  },
+});
