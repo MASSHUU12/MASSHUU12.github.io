fb3ccf2e17391abaeb8c6ff693ccdcbcb84900d0
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-01-18 19:58:02 +0100

    Build manifest automatically

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/",
+  build: {
+    manifest: true,
+  },
   plugins: [preact(), tsconfigPaths()],
   resolve: {
     alias: {
