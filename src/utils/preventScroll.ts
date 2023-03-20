68136c62f10c6fb2e5315b3ad44e17723dc0af64
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-03-20 17:12:11 +0100

    Better way to prevent scrolling page

--- src/utils/preventScroll.ts
+++ src/utils/preventScroll.ts
@@ -1,10 +1,10 @@
 export const scroll = {
   /** Remove class from body to enable page scrolling. */
   enable: (): void => {
-    document.querySelector("body")?.removeAttribute("class");
+    document.body.style.overflow = "";
   },
   /** Set class to body to prevent from page scrolling. */
   disable: (): void => {
-    document.querySelector("body")!.className = "disable-scroll";
+    document.body.style.overflow = "hidden";
   },
 };
