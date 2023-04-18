79fa3b56218410bf8e211edee4cf72eb8973d668
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-04-18 15:27:16 +0200

    Installed Tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@ import { render } from "preact";
 import { Provider } from "react-redux";
 
 import App from "./App";
-import "./i18n";
 import store from "./app/store";
 import About from "sections/about/About";
 import CV from "sections/cv/CV";
 import MobileMenu from "sections/mobileMenu/MobileMenu";
+
+import "./i18n";
+import "./index.css";
 
 render(
   <Provider store={store}>
