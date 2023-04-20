27d7b36461d4997976c0c1f815c7873ee9ae631f
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-04-20 13:08:11 +0200

    Simplified folders structure

--- src/components/sections/Main.tsx
+++ src/components/sections/Main.tsx
@@ -4,7 +4,6 @@ import { animated, useSpring } from "@react-spring/web";
 
 import Btn from "common/Btn";
 import Scroll from "helpers/Scroll";
-import { toggleCV } from "features/cvSlice";
 import { toggle } from "features/popupsSlice";
 import { useAppDispatch } from "src/app/hooks";
 import { slideToLeftAnim } from "src/animations/slideToLeftAnim";
