520d88313399a021ab1cd461568af7d032f7ee7c
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-04-20 13:06:21 +0200

    Removed unused elements from Redux store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,13 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import aboutReducer from "../features/aboutSlice";
-import cvReducer from "../features/cvSlice";
 import mobileMenuReducer from "../features/mobileMenuSlice";
 import popupsReducer from "../features/popupsSlice";
 
 const store = configureStore({
   reducer: {
-    about: aboutReducer,
-    cv: cvReducer,
     mobileMenu: mobileMenuReducer,
     popups: popupsReducer,
   },
