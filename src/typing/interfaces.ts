e1132ba2f99792c8205b8aed1315889e882bfc20
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-05-05 21:44:09 +0200

    Removed unused code

--- src/typing/interfaces.ts
+++ src/typing/interfaces.ts
@@ -1,4 +1,3 @@
-import { StateUpdater } from "preact/hooks";
 import { popups } from "./types";
 
 export interface InfoProps {
@@ -19,27 +18,6 @@ export interface InfoProps {
       | never[];
   };
   keyID: number;
-}
-
-// TODO: Delete this interface
-export interface InfoToggleProps {
-  item: {
-    title: string;
-    short_desc: string;
-    description: string;
-    labels: Array<string>;
-    labels_left: Array<string>;
-    teamwork: boolean;
-    images: Array<string>;
-    links:
-      | {
-          name: string;
-          type: string;
-          link: string;
-        }[]
-      | never[];
-  };
-  setToggle: StateUpdater<boolean>;
 }
 
 export interface Popups {
