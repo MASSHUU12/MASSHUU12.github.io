8cf1d7c675a51fe3d465b7775752664f8544f58b
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-09-06 11:03:14 +0200

    Create ProjectCategory interface

--- src/typing/interfaces.ts
+++ src/typing/interfaces.ts
@@ -1,4 +1,4 @@
-import { popups } from "./types";
+import { popups, projectCategory } from "./types";
 
 export interface InfoProps {
   item: {
@@ -23,6 +23,12 @@ export interface ProjectInfoProps extends InfoProps {
   // eslint-disable-next-line no-unused-vars
   setData: (data: InfoProps) => void;
   deleteData: () => void;
+}
+
+export interface ProjectCategory {
+  category: projectCategory;
+  // eslint-disable-next-line no-unused-vars
+  setCategory: (category: projectCategory) => void;
 }
 
 export interface Popups {
