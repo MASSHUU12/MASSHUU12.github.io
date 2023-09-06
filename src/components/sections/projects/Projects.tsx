02f938d0e3d812d56481510afb388afdcd2f2c65
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-09-06 12:01:40 +0200

    Change category in store

--- src/components/sections/projects/Projects.tsx
+++ src/components/sections/projects/Projects.tsx
@@ -1,9 +1,12 @@
 import { useTranslation } from "react-i18next";
 import { FunctionComponent, JSX } from "preact";
 
+import { InfoProps } from "@/typing/interfaces";
+import { projectCategory } from "@/typing/types";
+
 import Project from "./Project";
 import projects from "@/projects.json";
-import { InfoProps } from "@/typing/interfaces";
+import { useProjectCategoryStore } from "@/app/store";
 
 /**
  * Projects section.
@@ -11,7 +14,17 @@ import { InfoProps } from "@/typing/interfaces";
  * @return {*} {JSX.Element}
  */
 const Projects: FunctionComponent<unknown> = (): JSX.Element => {
+  const store = useProjectCategoryStore();
   const { t } = useTranslation();
+
+  /**
+   * Changes the category of the projects.
+   *
+   * @param e {Event} The event.
+   */
+  function changeCategory(e: Event): void {
+    store.setCategory((e.target as HTMLSelectElement).value as projectCategory);
+  }
 
   return (
     <section id="works" class="grid place-items-center gap-8">
@@ -21,7 +34,10 @@ const Projects: FunctionComponent<unknown> = (): JSX.Element => {
         <label for="category" class="sr-only">
           {t("wSelect")}
         </label>
-        <select class="px-4 py-2 text-base border-none rounded-md shadow-sm cursor-pointer bg-white_custom sm:text-sm">
+        <select
+          value={store.category}
+          onChange={e => changeCategory(e)}
+          class="px-4 py-2 text-base border-none rounded-md shadow-sm cursor-pointer bg-white_custom sm:text-sm">
           <option value="all" selected>
             {t("wCAll")}
           </option>
