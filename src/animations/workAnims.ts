7097a381961c5f4a5f6963cb58bf5c58cd3243fb
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-01-28 19:45:31 +0100

    Update animations

--- src/animations/workAnims.ts
+++ src/animations/workAnims.ts
@@ -1,39 +1,35 @@
 export const workSlideInAnimation = {
-  to: {
-    y: 0,
-    opacity: 1,
-  },
-  from: {
-    y: 100,
-    opacity: 0,
-  },
-  reset: true,
+	to: {
+		y: 0,
+		opacity: 1,
+	},
+	from: {
+		y: 100,
+		opacity: 0,
+	},
+	reset: true,
 };
 
-export const workBackgroundAnimation = (
-  reverse: boolean,
-  closeAnim: () => void,
-) => {
-  return {
-    to: {
-      scaleX: 1,
-      opacity: reverse ? 0.3 : 1,
-    },
-    from: {
-      scaleX: 0,
-      opacity: reverse ? 0 : 1,
-    },
-    reset: true,
-    reverse,
-    onRest: closeAnim,
-  };
+export const workBackgroundAnimation = (reverse: boolean, closeAnim: () => void) => {
+	return {
+		to: {
+			scaleX: 1,
+			opacity: reverse ? 0.3 : 1,
+		},
+		from: {
+			scaleX: 0,
+			opacity: reverse ? 0 : 1,
+		},
+		reset: true,
+		reverse,
+		onRest: closeAnim,
+	};
 };
 
-export const workItemAnimation = (animPlayed: boolean, mouseOver: boolean) => {
-  return {
-    opacity: animPlayed ? 1 : 0,
-    y: animPlayed ? 0 : 100,
-    scale: mouseOver ? 1.1 : 1,
-    reset: true,
-  };
+export const workItemAnimation = (animPlayed: boolean) => {
+	return {
+		opacity: animPlayed ? 1 : 0,
+		y: animPlayed ? 75 : 0,
+		reset: true,
+	};
 };
