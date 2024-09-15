55c4f4eaf207afb5f0b7f6e2e64386ce46f2be31
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-09-15 15:33:26 +0200

    refactor: create basic theme & partially rewrite Main & Footer

--- src/components/sections/Main.tsx
+++ src/components/sections/Main.tsx
@@ -1,47 +1,68 @@
-import { useTranslation } from "react-i18next";
-import { FunctionComponent, JSX } from "preact";
-import { animated, useSpring } from "@react-spring/web";
-
-import Scroll from "@/helpers/Scroll";
-import Btn from "@/components/common/Btn";
-import { usePopupsStore } from "@/app/store";
 import { slideToLeftAnim } from "@/animations/slideToLeftAnim";
+import { usePopupsStore } from "@/app/store";
+import Btn from "@/components/common/Btn";
+import Scroll from "@/helpers/Scroll";
+import { Stack } from "@mui/material";
+import { animated, useSpring } from "@react-spring/web";
+import { JSX } from "preact";
+import { useTranslation } from "react-i18next";
 
-const Main: FunctionComponent<unknown> = (): JSX.Element => {
-	const { t } = useTranslation();
-	const togglePopups = usePopupsStore(state => state.toggle);
+function Main(): JSX.Element {
+  const { t } = useTranslation();
+  const togglePopups = usePopupsStore(state => state.toggle);
 
-	const nameAnimation = useSpring({
-		...slideToLeftAnim,
-		delay: 300,
-	});
+  const nameAnimation = useSpring({
+    ...slideToLeftAnim,
+    delay: 300,
+  });
 
-	const titleAnimation = useSpring({
-		...slideToLeftAnim,
-		delay: 450,
-	});
+  const titleAnimation = useSpring({
+    ...slideToLeftAnim,
+    delay: 450,
+  });
 
-	const AnimatedH1 = animated.h1 as any;
-	const AnimatedSpan = animated.span as any;
+  const AnimatedH1 = animated.h1 as any;
+  const AnimatedSpan = animated.span as any;
 
-	return (
-		<>
-			<section class="flex flex-col gap-3 justify-center items-center h-svh">
-				<AnimatedH1 style={nameAnimation} class="text-4xl md:text-5xl text-white_custom md:text-center">
-					{t("meHello")}
-					<span class="text-light_yellow"> Maciej Gawrysiak</span>.
-				</AnimatedH1>
-				<AnimatedSpan style={titleAnimation} class="text-xl md:text-2xl text-gray">
-					{t("meTitle")}
-				</AnimatedSpan>
-				<div class="flex flex-wrap gap-3">
-					<Btn text={t("hAbout")} action={() => togglePopups("about")} delay={100} />
-					<Btn text={t("hWorks")} action={() => Scroll.intoView({ target: "#works" })} delay={250} />
-					<Btn text="CV" action={() => window.open("/assets/gawrysiak_cv.pdf", "_blank")} delay={400} />
-				</div>
-			</section>
-		</>
-	);
-};
+  return (
+    <>
+      <Stack
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+        gap={3}
+        sx={{ height: "100svh" }}>
+        <AnimatedH1
+          style={nameAnimation}
+          class="text-4xl md:text-5xl text-white_custom md:text-center">
+          {t("meHello")}
+          <span class="text-light_yellow"> Maciej Gawrysiak</span>.
+        </AnimatedH1>
+        <AnimatedSpan
+          style={titleAnimation}
+          class="text-xl md:text-2xl text-gray">
+          {t("meTitle")}
+        </AnimatedSpan>
+        <Stack direction="row" gap={3}>
+          <Btn
+            text={t("hAbout")}
+            action={() => togglePopups("about")}
+            delay={100}
+          />
+          <Btn
+            text={t("hWorks")}
+            action={() => Scroll.intoView({ target: "#works" })}
+            delay={250}
+          />
+          <Btn
+            text="CV"
+            action={() => window.open("/assets/gawrysiak_cv.pdf", "_blank")}
+            delay={400}
+          />
+        </Stack>
+      </Stack>
+    </>
+  );
+}
 
 export default Main;
