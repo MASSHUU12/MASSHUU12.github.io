6dfbb5edcbaf090f5e9701fd305aca72abef2e19
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-09-16 16:43:45 +0200

    chore: rework & simplify Projects component

--- src/components/sections/projects/Projects.tsx
+++ src/components/sections/projects/Projects.tsx
@@ -1,55 +1,76 @@
-import { useTranslation } from "react-i18next";
-import { FunctionComponent, JSX } from "preact";
-
+import { useProjectCategoryStore } from "@/app/store";
+import projects from "@/projects.json";
 import { InfoProps } from "@/typing/interfaces";
 import { projectCategory } from "@/typing/types";
-
+import {
+  Stack,
+  ToggleButton,
+  ToggleButtonGroup,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
+import { JSX } from "preact";
+import { useTranslation } from "react-i18next";
 import Project from "./Project";
-import projects from "@/projects.json";
-import { useProjectCategoryStore } from "@/app/store";
 
-const Projects: FunctionComponent<unknown> = (): JSX.Element => {
-	const store = useProjectCategoryStore();
-	const { t } = useTranslation();
+function Projects(): JSX.Element {
+  const store = useProjectCategoryStore();
+  const { t } = useTranslation();
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.up("sm"));
 
-	function changeCategory(e: Event): void {
-		store.setCategory((e.target as HTMLSelectElement).value as projectCategory);
-	}
+  const handleCategory = (
+    _: MouseEvent,
+    newCategory: projectCategory | null,
+  ): void => {
+    if (newCategory === null || store.category === newCategory) {
+      return;
+    }
 
-	function displayProject({ item, keyID }: InfoProps): JSX.Element {
-		if (item.category !== store.category && store.category !== "all") return <></>;
+    store.setCategory(newCategory);
+  };
 
-		return <Project key={keyID} item={item} keyID={keyID} />;
-	}
+  const displayProject = ({ item, keyID }: InfoProps): JSX.Element => {
+    if (item.category !== store.category && store.category !== "all") {
+      return <></>;
+    }
 
-	return (
-		<section id="works" class="grid place-items-center gap-4 mb-20">
-			{/* Category selection */}
-			<div class="flex flex-row items-center gap-6">
-				<h2 class="text-xl text-white_custom">{t("wProj")}</h2>
-				<label for="category" class="sr-only">
-					{t("wSelect")}
-				</label>
-				<select
-					value={store.category}
-					onChange={e => changeCategory(e)}
-					class="px-4 py-2 text-base text-black border-none rounded-md shadow-sm cursor-pointer bg-white_custom sm:text-sm">
-					<option value="all" selected>
-						{t("wCAll")}
-					</option>
-					<option value="web">{t("wCWeb")}</option>
-					<option value="mobile">{t("wCMob")}</option>
-					<option value="desktop">{t("wCDes")}</option>
-					<option value="extension">{t("wCEx")}</option>
-				</select>
-			</div>
+    return <Project key={keyID} item={item} keyID={keyID} />;
+  };
 
-			{/* Map through items and displays them. */}
-			{projects.map(
-				(item: InfoProps["item"], index: number): JSX.Element => displayProject({ item, keyID: index }),
-			)}
-		</section>
-	);
-};
+  return (
+    <Stack id="works" gap={1} mb={12} alignItems="center">
+      <ToggleButtonGroup
+        sx={{
+          backgroundColor: "lightYellow.main",
+        }}
+        size={`${matches ? "medium" : "small"}`}
+        exclusive
+        value={store.category}
+        onChange={handleCategory}
+        aria-label="Category selection">
+        <ToggleButton value="all" aria-label={`${t("wCAll")}`}>
+          {t("wCAll")}
+        </ToggleButton>
+        <ToggleButton value="web" aria-label={`${t("wCWeb")}`}>
+          {t("wCWeb")}
+        </ToggleButton>
+        <ToggleButton value="mobile" aria-label={`${t("wCMob")}`}>
+          {t("wCMob")}
+        </ToggleButton>
+        <ToggleButton value="desktop" aria-label={`${t("wCDes")}`}>
+          {t("wCDes")}
+        </ToggleButton>
+        <ToggleButton value="extension" aria-label={`${t("wCEx")}`}>
+          {t("wCEx")}
+        </ToggleButton>
+      </ToggleButtonGroup>
+      {projects.map(
+        (item: InfoProps["item"], index: number): JSX.Element =>
+          displayProject({ item, keyID: index }),
+      )}
+    </Stack>
+  );
+}
 
 export default Projects;
