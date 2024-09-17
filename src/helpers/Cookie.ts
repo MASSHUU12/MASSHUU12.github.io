dc98a8758035a2c3315ffdcaa094317fc9831cc1
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-09-17 19:42:07 +0200

    chore: add SameSite attribute for cookie

--- src/helpers/Cookie.ts
+++ src/helpers/Cookie.ts
@@ -1,18 +1,4 @@
-/**
- * Class for cookie management.
- *
- * @export
- * @class Cookie
- */
 export default class Cookie {
-  /**
-   * Get the value of a cookie by its name.
-   *
-   * @static
-   * @param {string} name - The name of the cookie.
-   * @return {*}  {string}
-   * @memberof Cookie The value of the cookie, or an empty string if it does not exist.
-   */
   public static getCookie(name: string): string {
     const cookie = document.cookie
       .split("; ")
@@ -22,20 +8,16 @@ export default class Cookie {
     return decodeURIComponent(cookie ?? "");
   }
 
-  /**
-   * Set the value for a cookie.
-   *
-   * @static
-   * @param {string} name - The name of the cookie.
-   * @param {string | number | boolean} value - The value for the cookie.
-   * @memberof Cookie
-   */
   public static setCookie(
     name: string,
     value: string | number | boolean,
   ): void {
     const age = 2_592_000; // a month
 
-    document.cookie = `${name}=${encodeURIComponent(value)};max-age=${age}`;
+    document.cookie = [
+      `${name}=${encodeURIComponent(value)}`,
+      `Max-Age=${age}`,
+      "SameSite=Strict",
+    ].join(";");
   }
 }
