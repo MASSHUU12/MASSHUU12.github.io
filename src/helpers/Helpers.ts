cd61142b16f830e78014f607beb55fe446201470
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-09-18 11:39:05 +0200

    chore: remove unneeded comments

--- src/helpers/Helpers.ts
+++ src/helpers/Helpers.ts
@@ -1,19 +1,7 @@
-/**
- * Check if the given string is a valid URL
- *
- * @param {string} url - The URL to check
- * @returns {boolean} - True if the URL is valid, false otherwise
- */
 export function isURL(url: string): boolean {
   return /^(http|https):\/\/[^ "]+$/.test(url);
 }
 
-/**
- * Create a valid image path
- *
- * @param {string} url - The URL to check
- * @returns {string} - The valid image path
- */
 export function createImagePath(url: string): string {
   return isURL(url) ? url : `/assets/projects/${url}`;
 }
